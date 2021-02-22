f01af6e338a7e3d477095ac97eb854585582d463
EDahm <2dahm@daum.net>
2021-02-22 14:44:19 +0900

    MemberVO import 뺌

--- src/main/java/org/zerock/controller/AuctionController.java
+++ src/main/java/org/zerock/controller/AuctionController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.Criteria;
-import org.zerock.domain.MemberVO;
 import org.zerock.domain.PageDTO;
 import org.zerock.domain.aucApplyVO;
 import org.zerock.domain.aucBidVO;
